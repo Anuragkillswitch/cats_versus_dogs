13e295d04b4b1a23f3ab7112c0211f7b12de25d0
Anuragkillswitch <70265851+Anuragkillswitch@users.noreply.github.com>
2021-06-28 15:34:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cats_versus_dogs
+ A cat versus Dog detector using TeachableMachine's Convolutional neural network using TensorFlowLite and flutter!
---
+++ android/app/src/main/kotlin/anurag/bandyopadhyay/com/cats_versus_dogs/MainActivity.kt
@@ -0,0 +1,6 @@
+package anurag.bandyopadhyay.com.cats_versus_dogs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
